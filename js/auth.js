9dc70f257ca647c146417c6c990c354f8e03b8b0
Paula Pirajón <est.paula.pirajon@unimilitar.edu.co>
2026-04-12 22:48:57 -0500

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,11 @@
+function cerrarSesiones() {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = "index.html";
+}
+
+function eliminarCuenta() {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = "index.html";
+}
