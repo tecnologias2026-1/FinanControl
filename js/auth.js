6c92c4f61304626d0679efd0c4046776e2f338b1
Paula Pirajón <est.paula.pirajon@unimilitar.edu.co>
2026-05-23 14:48:33 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,15 @@
-function cerrarSesiones() {
+function cerrarSesion() {
     localStorage.clear();
     sessionStorage.clear();
+
     window.location.href = "index.html";
 }
 
 function eliminarCuenta() {
     localStorage.clear();
     sessionStorage.clear();
+
+    alert("Cuenta eliminada");
+
     window.location.href = "index.html";
 }
